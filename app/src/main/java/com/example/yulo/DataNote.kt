1ee056c302247a9215bf0e167d66ed185cd74403
kaverisrinivas <kaverigajula318@gmail.com>
2020-02-11 16:24:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/yulo/DataNote.kt
@@ -0,0 +1,9 @@
+package com.example.yulo
+
+class DataNote (nodeID: Int, nodeName: String, nodeDes: String) {
+
+        var nodeID: Int? = nodeID
+        var nodeName: String? = nodeName
+        var nodeDes: String? = nodeDes
+
+    }
